db7d813e431e78261473cc94e35e18325f5e5632
Жолоб Алена Владимировна <a.zholob@khakasia.local>
2022-12-07 16:32:30 +0700

    add input 'login' and 'Password'

--- src/components/ui/loginForm.jsx
+++ src/components/ui/loginForm.jsx
@@ -1,7 +1,16 @@
 import React from "react"
 
 const LoginForm = () => {
-    return <h1>This is Login Form</h1>
+    return (
+        <>
+        <h1>This is Login Form</h1>
+        <label>Login</label>
+        <input type="text" name="login" id="login"></input>
+        <label>Password</label>
+        <input type="password" name="password" id="password"></input>
+        </>
+    
+    )
 }
  
 export default LoginForm;
